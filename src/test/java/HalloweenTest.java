79f1209f1f9e51068f68001029b53c191d660aa3
emuratalieva <emuratalieva@gmail.com>
2020-11-03 21:40:23 -0500

    no trick or treat

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -3,5 +3,12 @@ public class HalloweenTest {
     public static void main(String[] args) {
 
 // Im about to give candy for Halloween
+ // Hello HEllo
+
+ // some changes added
+
+//there will  be no trick or treat
+        // stay home
+
     }
 }
